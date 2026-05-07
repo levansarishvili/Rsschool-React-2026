cc604684f3b2990acc8c9adfc87222532851fc4e
Levani Sarishvili <levanisarishvili322@gmail.com>
2026-05-07 21:44:09 +0400

    chore(test): install and configure testing tools

---
+++ vitest.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: 'tests/setupTests.ts',
+
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+
+      thresholds: {
+        global: {
+          statements: 80,
+          branches: 50,
+          functions: 50,
+          lines: 50,
+        },
+      },
+
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/**/*.spec.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'src/**/*.d.ts',
+      ],
+    },
+  },
+});

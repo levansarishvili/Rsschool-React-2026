9026bba61b1b45f29815c612aefdfb4b68a8089f
Levani Sarishvili <levanisarishvili322@gmail.com>
2026-05-25 12:50:41 +0400

    chore: install and setup redux toolkit library

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App.tsx';
 import ErrorBoundary from './components/ErrorBoundary.tsx';
 import { ThemeProvider } from './contexts/ThemeProvider.tsx';
+import { Provider } from 'react-redux';
+import store from './store/store.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </Provider>
     </ErrorBoundary>
   </StrictMode>
 );
